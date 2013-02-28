8e3e5b47c6e3c9aa195c063faf102bc293f4bcc2
Andrew Carter <andrew.r.carter@gmail.com>
2013-02-28 13:18:04 -0500

    Initial Commit

---
+++ NSOperationNetworkDemo/PSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PSAppDelegate.h
+//  NSOperationNetworkDemo
+//
+//  Created by Andrew Carter on 2/28/13.
+//  Copyright (c) 2013 Pinch Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
